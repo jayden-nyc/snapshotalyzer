bf04692f6169c34f6d43ceb6d15842f92adb91fc
Jaycade <jaycade@ProMac-T.local>
2019-09-25 16:43:35 -0400

    Adding error handling with try and except.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -102,7 +103,11 @@ def start_instances(project):
 
     for i in instances:
         print('Starting {0}...'.format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print('Could not start {0} '.format(i.id) + str(e))
+            
     return
 
 
@@ -115,7 +120,11 @@ def stop_instances(project):
 
     for i in instances:
         print('Stopping {0}...'.format(i.id))
+    try:
         i.stop()
+    except botocore.exceptions.ClientError as e:
+        print('Could not stop {0} '.format(i.id) + str(e))
+
     return
 
 @instances.command('snapshot', help='Create snapshots of all volumes')
